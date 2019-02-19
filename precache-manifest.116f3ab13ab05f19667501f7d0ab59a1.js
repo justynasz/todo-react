6cf144464b9761b4eb4611007b129b6f1f3d1db7
justynasz <justynaszczygiel@outlook.com>
2019-02-19 10:02:50 +0100

    Updates

---
+++ precache-manifest.116f3ab13ab05f19667501f7d0ab59a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "966e5faaf0a6772e3706",
+    "url": "/static/js/main.966e5faa.chunk.js"
+  },
+  {
+    "revision": "e65d2ef46aa8be294d01",
+    "url": "/static/js/2.e65d2ef4.chunk.js"
+  },
+  {
+    "revision": "966e5faaf0a6772e3706",
+    "url": "/static/css/main.fc8cd0d3.chunk.css"
+  },
+  {
+    "revision": "07353840183649ebd3423916b82ab092",
+    "url": "/index.html"
+  }
+];
